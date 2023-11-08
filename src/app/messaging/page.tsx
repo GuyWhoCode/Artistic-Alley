751695363e5900e54bb26afb79b20df516743193
OJ101003 <hackerdude1010@gmail.com>
2023-11-08 15:11:06 -0800

    I hate errors

--- src/app/messaging/page.tsx
+++ src/app/messaging/page.tsx
@@ -1,17 +1,21 @@
 import Link from "next/link";
 import "../globals.css";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export default function Page() {
     return (
         <main>
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+            <div className="flex h-full justify-center pt-[100px]">
+                <ScrollArea className="h-[200px] w-[350px] rounded-md border p-4"></ScrollArea>
+                
+            </div>
+            <Link href="/">Return Home</Link>
             {/* 
             TODO:
             We need to build just the front end of the messaging page
             I think we're going to need components for the messaging boxes and we need to figure out how to change chats when u click
             Firstly if we just design the general layout that would be great
             */}
-            <Link href="/">Return Home</Link>
         </main>
     );
 }
