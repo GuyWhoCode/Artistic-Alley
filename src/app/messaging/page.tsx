4b2b21db70ecc5b63780dfbade2bfa8bd436cbf7
OJ101003 <hackerdude1010@gmail.com>
2023-11-13 23:04:32 -0800

    Fixed messaging page viewheight bug

--- src/app/messaging/page.tsx
+++ src/app/messaging/page.tsx
@@ -6,8 +6,9 @@ import ChatPreview from "@/components/chatPreview";
 export default function Page() {
     return (
         <main>
-            <div className="flex flex-col items-center h-full justify-center pt-[60px]">
+            <div className="flex flex-col items-center h-full justify-center pt-[60px] max-h-screen">
             <h1 className="text-3xl font-bold pb-6">Messaging</h1>
+            <Link href="/">Return Home</Link>
                 <ScrollArea className="h-[700px] w-[450px] rounded-md border p-4">
                     <ChatPreview profilePicture={"https://scontent-sjc3-1.xx.fbcdn.net/v/t1.6435-9/180978949_314228950059549_1005358403722529104_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=be3454&_nc_ohc=t-kEFO4r0oEAX8dCX0N&_nc_ht=scontent-sjc3-1.xx&oh=00_AfDDGu1dOSs-m8ToepSFqE3SCwGCN2ypyZgHjtUvibf2tQ&oe=6576618E"} userName={"Artistic"} currentCommision={"Questionable art for reasonable prices"}></ChatPreview>
                     <ChatPreview profilePicture={"https://scontent-sjc3-1.xx.fbcdn.net/v/t1.6435-9/180978949_314228950059549_1005358403722529104_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=be3454&_nc_ohc=t-kEFO4r0oEAX8dCX0N&_nc_ht=scontent-sjc3-1.xx&oh=00_AfDDGu1dOSs-m8ToepSFqE3SCwGCN2ypyZgHjtUvibf2tQ&oe=6576618E"} userName={"Artistic"} currentCommision={"Questionable art for reasonable prices"}></ChatPreview>
@@ -23,11 +24,8 @@ export default function Page() {
                     <ChatPreview profilePicture={"https://scontent-sjc3-1.xx.fbcdn.net/v/t1.6435-9/180978949_314228950059549_1005358403722529104_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=be3454&_nc_ohc=t-kEFO4r0oEAX8dCX0N&_nc_ht=scontent-sjc3-1.xx&oh=00_AfDDGu1dOSs-m8ToepSFqE3SCwGCN2ypyZgHjtUvibf2tQ&oe=6576618E"} userName={"Artistic"} currentCommision={"Questionable art for reasonable prices"}></ChatPreview>
                     <ChatPreview profilePicture={"https://scontent-sjc3-1.xx.fbcdn.net/v/t1.6435-9/180978949_314228950059549_1005358403722529104_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=be3454&_nc_ohc=t-kEFO4r0oEAX8dCX0N&_nc_ht=scontent-sjc3-1.xx&oh=00_AfDDGu1dOSs-m8ToepSFqE3SCwGCN2ypyZgHjtUvibf2tQ&oe=6576618E"} userName={"Artistic"} currentCommision={"Questionable art for reasonable prices"}></ChatPreview>
                     <ChatPreview profilePicture={"https://scontent-sjc3-1.xx.fbcdn.net/v/t1.6435-9/180978949_314228950059549_1005358403722529104_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=be3454&_nc_ohc=t-kEFO4r0oEAX8dCX0N&_nc_ht=scontent-sjc3-1.xx&oh=00_AfDDGu1dOSs-m8ToepSFqE3SCwGCN2ypyZgHjtUvibf2tQ&oe=6576618E"} userName={"Artistic"} currentCommision={"Questionable art for reasonable prices"}></ChatPreview>
-
                 </ScrollArea>
-                
             </div>
-            <Link href="/">Return Home</Link>
         </main>
     );
 }
