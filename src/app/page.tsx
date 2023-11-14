87c7de8a0f51e465f580c33e6e513e342085b864
OJ101003 <hackerdude1010@gmail.com>
2023-11-14 13:22:30 -0800

    Added Homepage component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,18 @@ import { Input } from "@/components/ui/input";
 import HomepageCommision from "@/components/homepageCommision";
 
 export default function Page() {
+    const pfpUrlPlaceholder = "https://scontent-sjc3-1.xx.fbcdn.net/v/t1.6435-9/180978949_314228950059549_1005358403722529104_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=be3454&_nc_ohc=t-kEFO4r0oEAX8dCX0N&_nc_ht=scontent-sjc3-1.xx&oh=00_AfDDGu1dOSs-m8ToepSFqE3SCwGCN2ypyZgHjtUvibf2tQ&oe=6576618E";
     return (
         <main>
-            <div className="flex flex-col items-center justify-center pt-[60px] max-h-screen">
+            <div className="flex flex-col items-center justify-center pt-[60px] min-w-[320px]">
                 <h1 className="pb-4 text-5xl font-bold">Discovery page</h1>
-                <Input className=" max-w-[380px] mb-3 " placeholder="Search" />
-                <div className="grid grid-cols-2 gap-4">
-                    <HomepageCommision imgSrc = "https://picsum.photos/200" userName ="User Name" price="Price $$$" title="Commision Title"/>
-                    <HomepageCommision imgSrc = "https://picsum.photos/200" userName ="User Name" price="Price $$$" title="Commision Title"/>
-                    <HomepageCommision imgSrc = "https://picsum.photos/200" userName ="User Name" price="Price $$$" title="Commision Title"/>
-                    <HomepageCommision imgSrc = "https://picsum.photos/200" userName ="User Name" price="Price $$$" title="Commision Title"/>
-
+                <Input className=" max-w-[480px] mb-3 " placeholder="Search" />
+                <div className="grid grid-cols-2 gap-4 px-2">
+                    <HomepageCommision imgSrc = "https://picsum.photos/200" userName ="User Name" price="Price $$$" title="Commision Title" profilePicture={pfpUrlPlaceholder}/>
+                    <HomepageCommision imgSrc = "https://picsum.photos/200" userName ="User Name" price="Price $$$" title="Commision Title" profilePicture={pfpUrlPlaceholder}/>
+                    <HomepageCommision imgSrc = "https://picsum.photos/200" userName ="User Name" price="Price $$$" title="Commision Title" profilePicture={pfpUrlPlaceholder}/>
+                    <HomepageCommision imgSrc = "https://picsum.photos/200" userName ="User Name" price="Price $$$" title="Commision Title" profilePicture={pfpUrlPlaceholder}/>
+                    <HomepageCommision imgSrc = "https://picsum.photos/200" userName ="User Name" price="Price $$$" title="Commision Title" profilePicture={pfpUrlPlaceholder}/>
                 </div>
             </div>
             {/* <Link href="/profile"> Profile </Link>
--- src/components/homepageCommision.tsx
+++ src/components/homepageCommision.tsx
@@ -6,16 +6,27 @@ export default function HomepageCommision({
     userName,
     price,
     title,
+    profilePicture,
 }: {
     imgSrc: string;
     userName: string;
     price: string;
     title: string;
+    profilePicture: string;
 }) {
-    const size = 180;
+    const size = 350;
     return (
-        <div>
-            <Image src={imgSrc} alt="test" width={size} height={size} />
+        <div className="flex flex-col border-2 relative border-black">
+            <div className="relative">
+                <Image src={imgSrc} alt="test" width={size} height={size}/>
+            </div>
+            <div className="">
+                <Avatar className="absolute bottom-0 left-0 mb-7">
+                    <AvatarImage src={profilePicture} />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+                <h1 className="pb-4 text-base border-1 border-black pl-10 md:text-xl lg:text-xl">{title}</h1>
+            </div>
         </div>
     );
 }
