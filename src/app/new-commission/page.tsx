3b1e01bca94a9584377ffe7a474e8e2a30f29955
GuyWhoCode <34526187+GuyWhoCode@users.noreply.github.com>
2023-11-24 22:41:13 -0800

    feat: initialize basic fields

--- src/app/new-commission/page.tsx
+++ src/app/new-commission/page.tsx
@@ -5,9 +5,23 @@ import { Commission } from "@/database/types";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import useCurrentUser from "@/hooks/useCurrentUser";
+import { db } from "@/database/firebase";
+
+const IMAGE_HOST =
+    "https://res.cloudinary.com/datgtai6b/image/upload/v1700168828/artistic-alley-uploads/";
+const getImageSource = (image: string) => image.split(IMAGE_HOST)[1];
+
+function createNewCommission(commission: Commission) {
+    console.log(commission)
+    // addDoc(collection(db, "commissions"), commission);
+}
+
+
+
 
 export default function Page() {
     const { signedIn, user } = useCurrentUser();
+    const [loading, setLoading] = useState(true);
     const [commissionInfo, setCommissionInfo] = useState<Commission>({
         userId: "",
         title: "",
@@ -19,17 +33,43 @@ export default function Page() {
         keywords: [],
         image: "",
     });
-
     const router = useRouter();
+    function handleSubmission() {
+        const updatedCommissionInfo = {
+            ...commissionInfo,
+            userId: user.uid, // Replace 'user.id' with the actual user ID
+        };
+        setCommissionInfo(updatedCommissionInfo);
+        createNewCommission(updatedCommissionInfo);
+    }
 
-    // if (!signedIn) {
-    //     router.push("/login");
-    //     return <></>;
-    // }
+    // https://res.cloudinary.com/datgtai6b/image/upload/v1700168828/artistic-alley-uploads/hom7hm5kjuq5tbpcedhj.jpg
 
     return signedIn ? (
         <main>
             <h1>Create New Commission</h1>
+
+            <input
+                type="text"
+                name="title"
+                id="title"
+                placeholder="Commission Title"
+            />
+            <br />
+            <textarea
+                name="description"
+                id="description"
+                placeholder="Commission Description"
+            />
+            {/* Upload Image component */}
+
+            <hr />
+            <br />
+            <label htmlFor="price">$</label>
+            <input type="number" name="price" id="price" placeholder="Price" />
+            <br />
+            <textarea name="categories" id="categories" placeholder="Categories"/>
+            <button onClick={handleSubmission}>Save</button>
         </main>
     ) : (
         <main>
