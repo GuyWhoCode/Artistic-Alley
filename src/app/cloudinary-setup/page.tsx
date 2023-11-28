eff33e73bea502ccdce3ead288e5ed7be6b636fb
GuyWhoCode <34526187+GuyWhoCode@users.noreply.github.com>
2023-11-27 17:01:44 -0800

    feat: refacotred imageUpload component

--- src/app/cloudinary-setup/page.tsx
+++ src/app/cloudinary-setup/page.tsx
@@ -1,14 +1,41 @@
 "use client";
-import { useState, ChangeEvent, FormEvent } from "react";
+import { useState, FormEvent } from "react";
 import Head from "next/head";
 import Link from "next/link";
 import ImageUploadPreview from "@/components/imageUploadPreview"; // to import the imageUploadPreview component
 
 export default function Page() {
-    const handleUploadSuccess = (data: any) => {
-        // handle successful upload
-        console.log("Image uploaded successfully!");
-    };
+    const [imageSrc, setImageSrc] = useState<string | undefined>("");
+    const [uploadData, setUploadData] = useState<any>(); // Type is currently set to 'any'. Replace 'any' with the type of your upload data that you want.
+    // Currently still works with type=any
+
+    // When the form is submitted, this function is called and uploads the image online to cloudinary.
+    async function handleOnSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+
+        const form = event.currentTarget;
+        const formData = new FormData(form);
+
+        //presets go here. The 2nd parameter is the folder the image is uploaded to.
+        formData.append("upload_preset", "artistic-alley-uploads");
+
+        // fetch request
+        const data = await fetch(
+            "https://api.cloudinary.com/v1_1/datgtai6b/image/upload",
+            {
+                method: "POST",
+                body: formData,
+            }
+        ).then((r) => r.json());
+
+        // prints out all the information data (json) about the image onto the screen
+        // the link(url) printed out on the screen is how you can view the uploaded image
+        setImageSrc(data.secure_url);
+        setUploadData(data);
+
+        // only prints out the secure url of the image onto the console
+        console.log("Secure URL:", data.secure_url);
+    }
 
     return (
         <div>
@@ -25,7 +52,23 @@ export default function Page() {
                 <h1>Cloudinary Image Uploader</h1>
 
                 <p>Upload your image online to Cloudinary!</p>
-                <ImageUploadPreview onUploadSuccess={handleUploadSuccess} />
+                <form
+                    method="post"
+                    encType="multipart/form-data"
+                    onSubmit={handleOnSubmit}
+                >
+                    <ImageUploadPreview
+                        imageSrc={imageSrc}
+                        setImageSrc={setImageSrc}
+                    />
+
+                    {/* Submit button */}
+                    {imageSrc && (
+                        <p>
+                            <button type="submit">Upload Files</button>
+                        </p>
+                    )}
+                </form>
             </main>
             <br></br>
 
