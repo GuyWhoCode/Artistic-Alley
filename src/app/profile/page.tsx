5a837904d5c8c8031137231f50b8f9514d5e12ea
Ruiyang1210W <wangruiyang1210@gmail.com>
2023-12-05 14:14:16 -0800

    favicon updates

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -30,6 +30,16 @@ const ProfilePage = () => {
     <main className="profilePage">
       <ProfileBackground /> {/* Add the ProfileBackground component here */}
       <div className="returnHome">
+
+        <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+        <meta name="msapplication-TileColor" content="#da532c"/>
+        <meta name="theme-color" content="#ffffff"/>
+        </head>
         <Link href="/">
         <div className="w-10 h-10 bg-red-500 rounded-full flex items-center justify-center cursor-pointer text-white text-2xl no-underline border-2 border-white">
         <span>&#8592;</span>
